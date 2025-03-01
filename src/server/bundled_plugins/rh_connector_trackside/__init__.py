a67b34cd2a828c59e6f3426ee513ff064463dc8d
Glenn Pringle <glenn.pringle@sniip.com>
2025-03-01 21:26:09 +1000

    add race number and round

--- src/server/bundled_plugins/rh_connector_trackside/__init__.py
+++ src/server/bundled_plugins/rh_connector_trackside/__init__.py
@@ -142,7 +142,7 @@ class TracksideConnector():
 
                 if round_data:
                     roundNumber = round_data['RoundNumber']
-                    self._rhapi.db.heat_alter(self._rhapi.race.heat, name="TS Heat: {} Round: {} RaceNumber: {}".format(self._rhapi.race.heat, roundNumber, raceNumber))
+                    self._rhapi.db.heat_alter(heat.id, name="TS Heat:{} {}-{}".format(self._rhapi.race.heat, roundNumber, raceNumber))
 
         #stage here maybe it creates race.
         self._rhapi.race.stage(start_race_args)
